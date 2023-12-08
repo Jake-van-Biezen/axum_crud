2cb89a4720ec45e0fdccc93cbbe39df23d679aff
Jake van Biezen <jake1998@hotmail.nl>
2023-12-08 11:44:37 +0100

    Create quote handler added

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use axum::http;
-use axum::routing::{get, Router};
+mod handlers;
+use axum::routing::{get, post, Router};
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
-    let app = Router::new().route("/", get(health));
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
+
+    let app = Router::new()
+        .route("/", get(handlers::health))
+        .route("/quotes", post(handlers::create_quote))
+        .with_state(pool);
+
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
         .await
         .unwrap();
     axum::serve(listener, app).await.unwrap();
     Ok(())
-}
-
-async fn health() -> http::StatusCode {
-    http::StatusCode::OK
 }
