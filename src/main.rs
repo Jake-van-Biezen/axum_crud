28b560106f2a5410413d8dbe72b4978d11741699
Jake van Biezen <jake.van.biezen@sltn.nl>
2023-12-16 13:20:22 +0100

    Add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 mod handlers;
 use axum::routing::{delete, get, post, put, Router};
 use sqlx::postgres::PgPoolOptions;
+use tower_http::trace::TraceLayer;
+use tower_http::trace::{self};
+use tracing::Level;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
     let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
 
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
@@ -18,11 +25,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route("/quotes", get(handlers::read_quotes))
         .route("/quotes/:id", put(handlers::update_quote))
         .route("/quotes/:id", delete(handlers::delete_quote))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        )
         .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
         .await
         .unwrap();
+    tracing::info!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
     Ok(())
 }
